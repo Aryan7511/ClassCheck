c50549fbf6657e275849cfa8c8b0c23ed1e13da5
aryan <comauro7511@gmail.com>
2024-07-09 01:15:03 +0530

    added middleware for protected routes

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+'use client';
 import { Button } from '@/components/ui/button';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <>
-      <div>This is my Page</div>
-      <Button>Button</Button>
-    </>
-  );
+  useEffect(() => {
+    redirect('/api/auth/login?post_login_redirect_url=/dashboard');
+  }, []);
+  return;
 }
